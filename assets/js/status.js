a496f4f76318acd735d7505e443b18df0272ebef
ChemistryX <starpiung@naver.com>
2019-12-29 02:17:21 +0900

    convert to jekyll

---
+++ assets/js/status.js
@@ -0,0 +1,39 @@
+---
+---
+
+const api = "https://mcapi.us/server/status?ip=";
+const host = "{{ site.mc_url }}";
+
+setTimeout(function() {
+    $.getJSON(api + host, function(json) {
+        let count = "";
+        let motd = "";
+
+        const isOnline = json.online;
+        const players = json.players.now;
+        const max = json.players.max;
+
+        if (isOnline) {
+            motd = json.motd;
+            let mapName = motd.split('»')[1].replace('«', '').replace(/§./gi, '');
+            console.log(mapName);
+            count += players;
+            fetchPlayerCount(count + "<small>/" + max + " players</small>");
+            fetchMapName(mapName);
+        } else {            
+            $("#fallback").html("Server is offline.")
+        }
+
+        
+    }).fail(function() {
+        console.error("Could not parse JSON from the API.");
+    });
+}, 500);
+
+function fetchPlayerCount(html) {
+    $("#playerCount").html(html);
+}
+
+function fetchMapName(html) {
+    $("#mapName").html(html);
+}
---
+++ index.md
@@ -0,0 +1,12 @@
+---
+layout: default
+---
+
+<div class="container">
+    <div class="jumbotron info">
+        <h1 class="heading">Overcast <span style="color:#99AAB5">Community</span></h1>
+        <p class="lead">The awesome server that lets you play <br />large varieties of Minecraft PvP anytime, anywhere.</p>
+        <hr class="my-4" />
+        <a class="btn btn-primary btn-play" href="/play">Play Now »</a>
+    </div>
+</div>
