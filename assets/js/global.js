6b44ad8aa39799225b49ee195af11754016a386b
ChemistryX <starpiung@naver.com>
2019-12-30 01:51:35 +0900

    Add dark mode support based on user's system settings

---
+++ assets/js/global.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    const matcher = window.matchMedia("(prefers-color-scheme: dark)");
+    window.addEventListener("load", update);
+    matcher.addEventListener("change", update);
+
+    function update() {
+        if (matcher.matches) {
+            console.log("d");
+            $("body").addClass("bg-dark");
+            $(".footer")
+                .find("a")
+                .css("color", "white");
+            $(".navbar").removeClass("navbar-light");
+            $(".navbar").addClass("navbar-dark");
+            $(".navbar").removeClass("bg-light");
+            $(".navbar").addClass("bg-dark");
+            $(".card").addClass("text-white");
+            $(".card").addClass("bg-dark");
+        } else {
+            console.log("l");
+            $("body").removeClass("bg-dark");
+            $(".footer")
+                .find("a")
+                .css("color", "");
+            $(".navbar").removeClass("navbar-dark");
+            $(".navbar").addClass("navbar-light");
+            $(".navbar").removeClass("bg-dark");
+            $(".navbar").addClass("bg-light");
+            $(".card").removeClass("text-white");
+            $(".card").removeClass("bg-dark");
+        }
+    }
+});
