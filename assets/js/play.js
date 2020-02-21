a91a51210ef6b68decf28f0fbcc07aa5d1b17c72
applenick <applenick@users.noreply.github.com>
2020-02-20 16:24:53 -0800

    Add play-fallback.js and warning about map tags
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- assets/js/play.js
+++ assets/js/play.js
@@ -27,114 +27,52 @@ const protocolVersionMap = {
 
 function getServerStatus({ serverAddress, apiUrl, mapImagesUrl, playerAvatarsUrl }) {
   $.getJSON(apiUrl + serverAddress, response => {
-    if (response.online) {
-      // REMOVED FOR Bungee Support, add back when fixed...
-      //const pgm = response.bukkit_extra.pgm;
-      //const currentMatch = pgm[Object.keys(pgm)[0]];
-      //const currentMap = currentMatch.map.name;
-
-      //Fetch map name from MOTD, then strip away other data
-      const motd = response.description;
-      const motdStr = JSON.stringify(motd);
-      const mapName = motdStr.substring(motdStr.lastIndexOf("»") + 4, motdStr.lastIndexOf("«") - 3)
-      const matchState = getStateName(motdStr.charAt(36));
+    if (response.online && response.bukkit_extra) {
+      const pgm = response.bukkit_extra.pgm;
+      const currentMatch = pgm[Object.keys(pgm)[0]];
+      const currentMap = currentMatch.map.name;
 
       const data = {
         supportedVersions: response.version.supportedVersions,
         onlinePlayerCount: response.players.online,
         maxPlayerCount: response.players.max,
         onlinePlayerSample: response.players.sample,
-        mapName,
-        matchState,
 
-        /* Removed because of Bungee - Add back later
         currentMatch,
         currentState: currentMatch.state,
         currentMap,
         currentObjective: currentMatch.map.objective,
         currentTags: currentMatch.map.tags,
-        */
 
-        currentMapImage: mapImagesUrl + mapName + '/map.png'
+        currentMapImage: mapImagesUrl + currentMap + '/map.png'
       }
 
-      setVersionSupport();
+      setVersionSupport(data.supportedVersions);
       setPlayerCount(data.onlinePlayerCount, data.maxPlayerCount);
-      setMatchInfoTemp(data.matchState, data.mapName, data.currentMapImage, data.onlinePlayerSample, data.onlinePlayerCount, playerAvatarsUrl);
+      setMatchInfo(
+        data.currentState,
+        data.currentMap,
+        data.currentMapImage,
+        data.currentTags,
+        data.onlinePlayerSample,
+        data.onlinePlayerCount,
+        playerAvatarsUrl
+      );
     }
   });
 }
 
-function getStateName(state){
-  switch(state){
-    case 'a': 
-      return "running"
-    case 'e':
-      return "starting"
-    case 'c':
-      return "finished"
-    case '7':
-        return "idle";
-  }
-}
-
-function setVersionSupport(){
-  $('#data-version-support').html(`1.8 - 1.15.2`);
-}
-
-/* Removed for Bungee Support - Add back when fixed
 function setVersionSupport(supportedVersions) {
-  //const minVersion = protocolVersionMap[Math.min(...supportedVersions)];
-  //const maxVersion = protocolVersionMap[Math.max(...supportedVersions)];
+  const minVersion = protocolVersionMap[Math.min(...supportedVersions)];
+  const maxVersion = protocolVersionMap[Math.max(...supportedVersions)];
 
   $('#data-version-support').html(`${minVersion} - ${maxVersion}`);
-} 
-*/
+}
 
 function setPlayerCount(onlinePlayerCount, maxPlayerCount) {
   $('#data-player-count').html(`${onlinePlayerCount}<small class="text-muted">/${maxPlayerCount}</small>`);
 }
 
-
-function setMatchInfoTemp(currentState,
-  currentMap,
-  currentMapImage,
-  onlinePlayerSample,
-  onlinePlayerCount,
-  playerAvatarsUrl) {
-
-  $('#data-match-state').addClass(currentState);
-  $('#data-match-state').attr('title', currentState.charAt(0).toUpperCase() + currentState.slice(1)).tooltip({});
-  
-
-  var playersHTML = '';
-  if (onlinePlayerSample != null) {
-    onlinePlayerSample.forEach((player, index) => {
-      playersHTML += `
-        <img src="${playerAvatarsUrl}${player.id}?overlay&size=40" data-toggle="tooltip" data-placement="top" title="${player.name}" alt="${player.name}">
-      `;
-    });  
-
-    var morePlayersHTML = '';
-    if (onlinePlayerCount > onlinePlayerSample.length) {
-      morePlayersHTML = `
-        <span class="badge badge-info">
-          And ${onlinePlayerCount - onlinePlayerSample.length} more...
-        </span>
-      `;
-    }  
-  }
-
-
-  $('#data-current-map-image').attr('src', currentMapImage);
-  $('#data-current-map-name').html(currentMap);
-  $('#data-online-player-sample').html(playersHTML);
-  $('#data-more-player-count').html(morePlayersHTML);
-
-  $('#data-online-player-sample img').tooltip();
-}
-
-/* Removed for Bungee Support - Add back when fixed
 function setMatchInfo(currentState,
                       currentMap,
                       currentMapImage,
@@ -178,4 +116,3 @@ function setMatchInfo(currentState,
 
   $('#data-online-player-sample img').tooltip();
 }
-*/
